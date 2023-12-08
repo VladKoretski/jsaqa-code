32c375465a6b776f153b6ace83ebc071b6bb1c7a
VladKo <vpk1973@yandex.ru>
2023-12-08 15:50:23 +0300

    task 7_03

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,29 @@
 const sorting = require("../../app");
 
-describe("Books names test suit", () => {
+describe("Test 1. Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const books = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+    const expected = sorting.sortByName(books);
+    const result = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("Test 2. Books with the same names test suit", () => {
+  it("Books with the same names should be kept unordered", () => {
+    const books = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+    const expected = sorting.sortByName(books);
+    const result = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+
+    expect(result).toEqual(expected);
   });
 });
