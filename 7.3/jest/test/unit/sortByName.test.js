bd2c241fb1b385a3830c358a4bc08a46b0fc66cd
Vladimir Koretskiy <130839671+VladKoretski@users.noreply.github.com>
2023-12-08 15:53:16 +0300

    Update sortByName.test.js

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -20,9 +20,17 @@ describe("Test 1. Books names test suit", () => {
 
 describe("Test 2. Books with the same names test suit", () => {
   it("Books with the same names should be kept unordered", () => {
-    const books = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+    const books = [
+      "Гарри Поттер",
+      "Гарри Поттер",
+      "Гарри Поттер"
+    ];
     const expected = sorting.sortByName(books);
-    const result = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+    const result = [
+      "Гарри Поттер",
+      "Гарри Поттер",
+      "Гарри Поттер"
+    ];
 
     expect(result).toEqual(expected);
   });
